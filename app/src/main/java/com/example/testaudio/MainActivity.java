243dd327f505c782d1c737cef3b611eba7ff71fe
mosayeb <mb.masoumi1000@gmail.com>
2020-12-23 17:39:35 +0330

    first

---
+++ app/src/main/java/com/example/testaudio/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.testaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+ //   https://www.youtube.com/watch?v=Z4DQdeMAJRE
+
+    ImageView imagePlayPause;
+    TextView textCurrentTime, textTotalDuration;
+    SeekBar playerSeekbar;
+    MediaPlayer mediaPlayer;
+
+    Handler handler = new Handler();
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imagePlayPause = findViewById(R.id.imagePlayPause);
+        textCurrentTime = findViewById(R.id.textCurrentTime);
+        textTotalDuration = findViewById(R.id.textTotalDuration);
+        playerSeekbar = findViewById(R.id.playerSeekbar);
+        imagePlayPause = findViewById(R.id.imagePlayPause);
+
+        mediaPlayer = new MediaPlayer();
+
+        playerSeekbar.setMax(100);
+
+
+
+        imagePlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+               if(mediaPlayer.isPlaying()){
+
+                   handler.removeCallbacks(updater);
+                   mediaPlayer.pause();
+                   imagePlayPause.setImageResource(R.drawable.play);
+
+               }else{
+
+                   mediaPlayer.start();
+                   imagePlayPause.setImageResource(R.drawable.pause);
+                   updateSeekbar();
+               }
+                
+            }
+        });
+
+
+        prepareMediaPlayer();
+
+        playerSeekbar.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                SeekBar seekBar = (SeekBar) view;
+                int playPosition = (mediaPlayer.getDuration() / 100)*seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                textCurrentTime.setText(milliSecondToTimer(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+
+
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+                playerSeekbar.setSecondaryProgress(i);
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                 playerSeekbar.setProgress(0);
+                 imagePlayPause.setImageResource(R.drawable.play);
+                 textCurrentTime.setText("0:00");
+                 textTotalDuration.setText("0:00");
+                 mediaPlayer.reset();
+                 prepareMediaPlayer();
+            }
+        });
+
+    }
+
+    private void prepareMediaPlayer(){
+        try {
+
+            mediaPlayer.setDataSource("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_2MG.mp3");
+            mediaPlayer.prepare();
+            textTotalDuration.setText(milliSecondToTimer(mediaPlayer.getDuration()));
+
+        }catch (Exception exception){
+
+            Toast.makeText(this, ""+exception.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            updateSeekbar();
+            long currentDuration = mediaPlayer.getCurrentPosition();
+            textCurrentTime.setText(milliSecondToTimer(currentDuration));
+        }
+    };
+
+    private void updateSeekbar(){
+        if(mediaPlayer.isPlaying()){
+            playerSeekbar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) *100 ));
+            handler.postDelayed(updater , 1000);
+        }
+    }
+
+
+    private String milliSecondToTimer (long milliSeconds){
+
+        String timerString ="";
+        String secondsString;
+        int hour = (int) (milliSeconds/(1000*60*60));
+        int minutes = (int) (milliSeconds % (1000*60*60)) /(1000*60);
+        int seconds = (int) ((milliSeconds % (1000*60*60)) %(1000*60)/1000);
+
+        if(hour > 0){
+            timerString =  hour + ":";
+        }
+
+        if(seconds<10){
+            secondsString = "0" + seconds;
+        }else{
+            secondsString = "" + seconds;
+        }
+
+        timerString = timerString + minutes + ":" + secondsString;
+        return timerString;
+
+    }
+
+
+}
